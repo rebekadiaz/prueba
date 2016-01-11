4ae0af4d0c3749316d91b788062c7ce40b270ca8
Rebeca Díaz Rodriguez <girl_bcn2@hotmail.com>
2016-01-11 23:02:06 +0100

    valida 1

--- pruebagit/AppDelegate.m
+++ pruebagit/AppDelegate.m
@@ -14,6 +14,8 @@
 
 @implementation AppDelegate
 
+   //modif 1
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
